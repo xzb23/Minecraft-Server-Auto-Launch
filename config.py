c49740129a7f27cc147321ea53470bc9146f92f1
xz <xzbilibili233@163.com>
2026-03-08 22:45:51 +0800

    调整默认模式为v1

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ RCON_HOST = '127.0.0.1' # 服务器IP，本地则为127.0.0.1
 RCON_PORT = 25575 # RCON端口 (在server.properties中设置)
 RCON_PASSWORD = 'testpassword' # RCON密码 (在server.properties中设置)
 CHECK_INTERVAL = 10 # 检查在线人数的时间间隔，单位为秒
-MODE = "v2" # 运行模式，v1模式只要检测到连接就自动开启服务端，v2模式需要验证KEY
+MODE = "v1" # 运行模式，v1模式只要检测到连接就自动开启服务端，v2模式需要验证KEY
 KEY = b"testkey" # 验证KEY，仅v2模式使用
 STOP_FLAG = b"stoptestkey" # 关闭服务端的KEY，v1,v2模式均可使用
 
